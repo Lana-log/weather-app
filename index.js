268fcb694ab612a0315ed9ab238fe4f9a27b4d73
Lana-log <120496011+Lana-log@users.noreply.github.com>
2022-12-19 20:15:21 +0200

    update index.js (chose current city from weather app)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let currentTime = new Date();
 function formatDate(date) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
   let months = [
     "Jan",
     "Feb",
@@ -16,7 +15,6 @@ function formatDate(date) {
     "Nov",
     "Dec",
   ];
-
   let currentYear = date.getFullYear();
   let currentDay = days[date.getDay()];
   let currentMonth = months[date.getMonth()];
@@ -29,9 +27,7 @@ function formatDate(date) {
   if (currentMinutes < 10) {
     currentMinutes = `0${currentMinutes}`;
   }
-
   let formattedDate = `${currentHours}:${currentMinutes} ${currentDay} ${currentMonth} ${currentDate} ${currentYear}`;
-
   return formattedDate;
 }
 let date = document.querySelector("#currentDate");
@@ -41,9 +37,7 @@ date.innerHTML = formatDate(currentTime);
 function search(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = cityInput.value;
-  let city = cityInput.value;
+    let city = cityInput.value;
 let units = "metric";
 let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
@@ -56,6 +50,8 @@ form.addEventListener("click", search);
 function showTemperature(response) {
   let temperature = document.querySelector("#current-temperature");
   temperature.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.name;
   let description = document.querySelector("#description")
   description.innerHTML = response.data.weather[0].description;
 }
